cd476583d358c98e55f35bd8e4c1e60701b4d074
HenryAler <Anderson-fin@mail.ru>
2024-04-11 13:28:15 +0300

    start test index.html, base migrate, reworking urls.py

--- fruitbase/products/models.py
+++ fruitbase/products/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 class Product(models.Model):
 
-    name = models.CharField(max_length=50)
-    weight = models.FloatField()
-    price = models.FloatField()
-    date = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=50, verbose_name='Имя')
+    weight = models.FloatField(verbose_name='Вес (килограмм)')
+    price = models.FloatField(verbose_name='Цена')
+    date = models.DateTimeField(auto_now_add=True, verbose_name='Дата')
 
     def __str__(self):
         return self.name
