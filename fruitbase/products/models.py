894293599ede532b5c0503f2e8a8d551b9f05b80
HenryAler <Anderson-fin@mail.ru>
2024-04-11 13:05:48 +0300

    Add models product

--- fruitbase/products/models.py
+++ fruitbase/products/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+
+    name = models.CharField(max_length=50)
+    weight = models.FloatField()
+    price = models.FloatField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta():
+        ordering = ['date']
